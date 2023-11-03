e995f6ea7750908457016c2d7f3f9ba55c2cce08
Bruno Monteiro <brunobapmon@gmail.com>
2023-11-03 11:06:02 -0300

    fix: no import_map file

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,21 +2,21 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$0 from "./sections/Hero.tsx";
-import * as $$$$$$1 from "./sections/Theme.tsx";
-import * as $$$$$$2 from "./sections/Footer.tsx";
+import * as $$$$$$0 from "./sections/Footer.tsx";
+import * as $$$$$$1 from "./sections/Hero.tsx";
+import * as $$$$$$2 from "./sections/ParagraphList.tsx";
 import * as $$$$$$3 from "./sections/SimpleTextList.tsx";
-import * as $$$$$$4 from "./sections/ParagraphList.tsx";
+import * as $$$$$$4 from "./sections/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
 const manifest = {
   "sections": {
-    "deco-sites/daniela-tombini/sections/Footer.tsx": $$$$$$2,
-    "deco-sites/daniela-tombini/sections/Hero.tsx": $$$$$$0,
-    "deco-sites/daniela-tombini/sections/ParagraphList.tsx": $$$$$$4,
+    "deco-sites/daniela-tombini/sections/Footer.tsx": $$$$$$0,
+    "deco-sites/daniela-tombini/sections/Hero.tsx": $$$$$$1,
+    "deco-sites/daniela-tombini/sections/ParagraphList.tsx": $$$$$$2,
     "deco-sites/daniela-tombini/sections/SimpleTextList.tsx": $$$$$$3,
-    "deco-sites/daniela-tombini/sections/Theme.tsx": $$$$$$1,
+    "deco-sites/daniela-tombini/sections/Theme.tsx": $$$$$$4,
   },
   "apps": {
     "deco-sites/daniela-tombini/apps/decohub.ts": $$$$$$$$$$$0,
